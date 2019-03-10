8ceeb822836e04abe7182d841df3f4b201ad49fc
Kudo <kudo@example.com>
2019-03-10 17:58:28 +0900

    入力された郵便番号から住所を出力できるようにしてmainに変更

--- address_search.py
+++ address_search.py
@@ -1,17 +1,17 @@
 import requests
 
-if __name__ == '__main__':
+
+def main():
     post_code = input("郵便番号(7ケタ)?: ")
     zipcode = post_code
-
     url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-
     r = requests.get(url)
-
     address_dict = r.json()
-
     address1 = address_dict["results"][0]["address1"]
     address2 = address_dict["results"][0]["address2"]
     address3 = address_dict["results"][0]["address3"]
-
     print(f"{address1}{address2}{address3}")
+
+
+if __name__ == '__main__':
+    main()
